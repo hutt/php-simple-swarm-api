6cf3e03de90b5344fa9261e67e5e0eeff25e78a2
hutt <jannis@hutt.io>
2016-06-08 11:50:37 +0200

    modified config.

--- config.php
+++ config.php
@@ -21,6 +21,6 @@ define('LOGIN_ACTION_URL', 'https://www.swarmapp.com/login');
 define('CACHE_FILE', 'cache.txt');
 
 //timespan swarm shall be cached
-define('TTL', 1800);
+define('TTL', 1800); //1800 = 30min
 
 ?>
